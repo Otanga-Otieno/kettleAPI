8b2f778f9ec414622e9e34497d2b28f60060df7f
Otanga-Otieno <otieno.otanga@gmail.com>
2022-10-10 01:18:33 +0300

    Add wiki

---
+++ wiki/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo nl2br("<h3>/api.php/inventory</h3>");
+echo nl2br("Return all inventory items (stock_id, description, price)");
+
+echo nl2br("\n\n");
+
+echo nl2br("<h3>/api.php/inventory/{stock_id}</h3>");
+echo nl2br("Return stock item details (description, price)");
